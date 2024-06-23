e6870702c692ea78e0c3addbcb64821f65f6eea0
mudmeehnn <mudmeelec@gmail.com>
2024-06-23 23:13:03 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 # config.py
-GOOGLE_SHEETS_CREDENTIALS = 'credentials.json'
+import os
+import json
+
+credentials_json = os.getenv('GOOGLE_CREDENTIALS')
+GOOGLE_SHEETS_CREDENTIALS = json.loads(credentials_json)
+
+# GOOGLE_SHEETS_CREDENTIALS = 'credentials.json'
 USER_SPREADSHEET_NAME = 'UserDatabase'
 TASK_SPREADSHEET_NAME = 'TasksDatabase'
