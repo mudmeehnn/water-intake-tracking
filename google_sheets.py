28b9a4d56694302b0bb2c05908e1afdfa62d6ba8
mudmeehnn <mudmeelec@gmail.com>
2024-06-26 11:05:39 +0700

    Update google_sheets.py

--- google_sheets.py
+++ google_sheets.py
@@ -1,7 +1,7 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from config import GOOGLE_SHEETS_CREDENTIALS, TASK_SPREADSHEET_NAME
-from water_intake import IntakeLog
+from water_intake import OldIntakeLog, NewIntakeLog
 
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 creds = ServiceAccountCredentials.from_json_keyfile_name(GOOGLE_SHEETS_CREDENTIALS, scope)
@@ -14,7 +14,13 @@ def sync_intake_logs_to_sheets():
     sheet.append_row(['Date', 'Amount', 'Daily Goal'])
     previous_goal = None
     
-    for log in IntakeLog:
+    for log in OldIntakeLog:
+        if log['daily_goal']:
+            previous_goal = log['daily_goal']
+        
+        sheet.append_row([log['date'].strftime("%Y-%m-%d"), log['amount_cups'], log.get('daily_goal') or previous_goal])
+
+    for log in NewIntakeLog:
         if log['daily_goal']:
             previous_goal = log['daily_goal']
         
